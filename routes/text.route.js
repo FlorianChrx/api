1ed90ec06d3441088048fbcc1f0e1df07dc7315a
Florian Chiraux <florian.chiraux@kpf-si.fr>
2021-10-20 17:33:29 +0200

    Use Response type return

--- routes/text.route.js
+++ routes/text.route.js
@@ -1,9 +1,11 @@
+const { Response } = require('../model/response');
+
 module.exports = app => {
     let router = require('express').Router();
     const textController = require('../controllers/text.controller')
 
     router.get('/:tag', async (request, response) => {
-        response.send(await textController.getText(request.params.tag))
+        response.send(new Response(true, "Message available in response data !" ,await textController.getText(request.params.tag)));
     })
 
     app.use('/api/text', router)
