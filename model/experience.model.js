ad908e831311cd9b567e0063ca5552ceaccc8eb9
Florian Chiraux <florian.chiraux@kpf-si.fr>
2021-10-21 17:56:28 +0200

    Update experience.model.js

--- model/experience.model.js
+++ model/experience.model.js
@@ -5,7 +5,8 @@ exports.Experience = orm.define('Experience', {
     // Model attributes are defined here
     id: {
         type: DataTypes.BIGINT,
-        primaryKey: true
+        primaryKey: true,
+        autoIncrement: true
     },
     start: {
         type: DataTypes.DATE,
@@ -13,13 +14,13 @@ exports.Experience = orm.define('Experience', {
     },
     end: {
         type: DataTypes.DATE
-    }, 
+    },
     label: {
         type: DataTypes.STRING
-    }, 
+    },
     description: {
         type: DataTypes.STRING(1024)
-    }, 
+    },
     company: {
         type: DataTypes.STRING,
         allowNull: false
