986fcfff920f3f963769fa0405467232f7c0b34a
FlorianChrx <43887259+FlorianChrx@users.noreply.github.com>
2021-10-21 18:01:43 +0200

    Generic status and response sending (#13)
    * Update router.js
    
    * Update router.js
    
    * 0.0.3

--- routes/router.js
+++ routes/router.js
@@ -1,7 +1,22 @@
+const { Response } = require('../model/response.model');
+
 /**
  * Init all http routing
  */
 exports.initRoutes = app => {
     require('./test.route')(app);
     require('./text.route')(app);
+}
+
+/**
+ * Send the API response with right status
+ * @param {Response} response the API response
+ * @param {*} httpResponse the http response
+ */
+exports.send = function (response, httpResponse) {
+    if (response.success) {
+        httpResponse.send(response);
+    } else {
+        httpResponse.status(500).send(response);
+    }
 }
