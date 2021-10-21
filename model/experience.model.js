20f7c78abe6422609489b51cceb207adb025c9db
Florian Chiraux <florian.chiraux@kpf-si.fr>
2021-10-21 14:05:03 +0200

    Model created

---
+++ model/experience.model.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require('sequelize');
+const { orm } = require('../orm/sequelize');
+
+exports.Experience = orm.define('Experience', {
+    // Model attributes are defined here
+    id: {
+        type: DataTypes.BIGINT,
+        primaryKey: true
+    },
+    start: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    end: {
+        type: DataTypes.DATE
+    }, 
+    label: {
+        type: DataTypes.STRING
+    }, 
+    description: {
+        type: DataTypes.STRING(1024)
+    }, 
+    company: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    tableName: 'experience'
+});
