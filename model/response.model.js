d936353fe51e518e252e4aa333c37dc7433e156f
Florian Chiraux <florian.chiraux@kpf-si.fr>
2022-03-18 09:25:09 +0100

    Delete getters and setters
    Deletes accessors causing wrong schema in front-end

--- model/response.model.js
+++ model/response.model.js
@@ -1,32 +1,8 @@
 module.exports = class ApiResponse {
     constructor(isSuccess, message, data) {
-        this._isSuccess = isSuccess;
-        this._message = message;
-        this._data = data;
-        if (!message) this._message = this.isSuccess ? 'Successfully done !' : 'An error has occurred';
-    }
-
-    get isSuccess() {
-        return this._isSuccess;
-    }
-
-    set isSuccess(value) {
-        this._isSuccess = value;
-    }
-
-    get message() {
-        return this._message;
-    }
-
-    set message(value) {
-        this._message = value;
-    }
-
-    get data() {
-        return this._data;
-    }
-
-    set data(value) {
-        this._data = value;
+        this.isSuccess = isSuccess;
+        this.message = message;
+        this.data = data;
+        if (!message) this.message = this.isSuccess ? 'Successfully done !' : 'An error has occurred';
     }
 }
